7e3f3d2bad7b71858e58f93e1d5ee3da4b059062
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-06-02 09:47:19 -0400

    if statement for key value in url

--- hello/app.py
+++ hello/app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if "name" in request.args:
+      name=request.args["name"] 
+    else:
+      name="world"
+    return render_template("index.html", name= name)
