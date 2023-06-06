c12fc152493042b61bc648447b1a8a81db76d83d
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-06-06 08:22:57 -0400

    layout.html ,flask

--- hello/app.py
+++ hello/app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  name = request.args.get("name","world")
-  return render_template("index.html", name= name)
+  return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+  return render_template("greet.html", name = request.args.get("name","world"))
