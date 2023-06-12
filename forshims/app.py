a884b1b08ad30c0af1f13aa039ffb801a40a95d2
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-06-12 16:53:57 -0400

    form security element cannot be changed:

--- forshims/app.py
+++ forshims/app.py
@@ -3,21 +3,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 REGISTRANTS = {}
+SPORTS = ["soccer","batminton","basketball"]
 
 @app.route("/")
 def index ():
-  return render_template("index.html")
+  return render_template("index.html" , sports= SPORTS)
 
 @app.route("/register",methods=["POST"])
 def register():
   name = request.form.get("name")
+  
   sport = request.form.get("sports")
-  REGISTRANTS[name] = sport
-                        
-
+  
+  
+  REGISTRANTS[name] = sport                      
   return render_template("success.html")
 
 
 @app.route("/registrant")
 def registrant():
-  return render_template("registrant.html", registrant= REGISTRANTS)
+  return render_template("registrant.html", registrants =  REGISTRANTS)
